54b602e7de5b29aa3673a0c606d96938e46a3d04
David Rolón <rolondavid@outlook.com>
2023-05-16 10:31:16 -0300

    Creacion base del proyecto

---
+++ src/test/java/com/david/foro_alura/ForoAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.foro_alura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
